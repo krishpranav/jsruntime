2b16ae3707dac7f4a7023679beda8235ccfe2da7
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-24 12:58:51 +0530

    lib: repl + root

--- lib/repl.go
+++ lib/repl.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/lithdew/quickjs"
 )
 
 func Repl() {
@@ -11,7 +13,7 @@ func Repl() {
 	fmt.Println("exit using ctrl-c or exit()")
 
 	for true {
-		fmt.Print(">> ")
+		fmt.Print("> ")
 
 		reader := bufio.NewReader(os.Stdin)
 		text, _ := reader.ReadString('\n')
@@ -20,4 +22,32 @@ func Repl() {
 		fmt.Println(Eval(text, &stringToEval))
 		stringToEval += ";undefined;"
 	}
+}
+
+func Eval(text string, buffer *string) string {
+	runtime := quickjs.NewRuntime()
+	defer runtime.Free()
+
+	ctx := runtime.NewContext()
+	defer ctx.Free()
+
+	globalsEval := ctx.Globals()
+	globalsEval.Set("__dispatch", ctx.Function(Globals))
+
+	k, errorInjectingGlobals := ctx.Eval(codeGlobals)
+	JSError(errorInjectingGlobals, false)
+	defer k.Free()
+
+	result, err := ctx.Eval(*buffer + text)
+
+	if err != nil {
+		JSError(err, false)
+		return result.String()
+	}
+
+	*buffer += fmt.Sprintf(";undefined; %s", text)
+
+	result.Free()
+
+	return result.String()
 }
---
+++ lib/root.go
@@ -0,0 +1,3 @@
+package lib
+
+var codeGlobals string

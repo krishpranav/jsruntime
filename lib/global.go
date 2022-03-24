48ff078a12e7d7f1becf8722d2b1664bf19f0067
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-24 12:34:59 +0530

    lib: global

--- lib/global.go
+++ lib/global.go
@@ -2,14 +2,19 @@ package lib
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/lithdew/quickjs"
 )
 
 func Globals(ctx *quickjs.Context, this quickjs.Value, args []quickjs.Value) quickjs.Value {
+
 	switch args[0].String() {
+
 	case "console":
 		fmt.Println(args[1].String())
+	case "close":
+		os.Exit(1)
 	}
 
 	return ctx.Null()
